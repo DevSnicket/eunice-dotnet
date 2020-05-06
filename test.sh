38f94cd7ecf0eac064a9a9a95686701cb70f0472
Graham Dyson <git@devsnicket.com>
2020-05-06 11:33:36 +0100

    test script coverage threshold errors raised from report generator instead of altcover altcover was only measuring statement and its branch calculation is wrong

--- test.sh
+++ test.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+set -e
+
 dotnet test \
 Analysis/Files/Tests \
 -l "trx;LogFileName=.trx" \
 -p:AltCover=true \
 -p:AltCoverAssemblyFilter="^(?!DevSnicket.Eunice.Analysis.Files$)" \
 -p:AltCoverForce=true \
--p:AltCoverThreshold=100 \
 -p:AltCoverXmlReport=TestResults/coverage.xml
-
-testExitCode=$?
 
 # ignore error raised when already installed
 dotnet tool install dotnet-reportgenerator-globaltool \
 --tool-path . \
 --version 4.5.6 \
+|| true # ignore error raised when already installed
 
 ./reportgenerator \
 -reports:Analysis/Files/Tests/TestResults/coverage.xml \
+-reporttypes:"Html;JsonSummary" \
 -targetdir:Analysis/Files/Tests/TestResults/CoverageReport
 
-exit $(($? + $testExitCode))
+grep -Pzo '"linecoverage": 100[^}]*"branchcoverage": 100' Analysis/Files/Tests/TestResults/CoverageReport/Summary.json
